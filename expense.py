a00d4df21470f1b111e36155cd01c6bdf5db3e5d
root <root@Labtop-nathan>
2023-09-20 21:38:47 +0200

    Partie 1

--- expense.py
+++ expense.py
@@ -1,5 +1,17 @@
 from PyInquirer import prompt
+from user import chose_user
+import csv
 
+
+possible_usernames = chose_user()
+possible_users = [
+    {
+    'name': user,
+    'value': user,
+    'checked': True,
+    }
+    for user in possible_usernames
+]
 expense_questions = [
     {
         "type":"input",
@@ -12,19 +24,28 @@ expense_questions = [
         "message":"New Expense - Label: ",
     },
     {
-        "type":"input",
+        "type":"list",
         "name":"spender",
         "message":"New Expense - Spender: ",
+        "choices": chose_user()
     },
-
+    {
+        "type":"checkbox",
+        "name":"involved_users",
+        "message":"New Expense - Involved Users",
+        "choices": possible_users,
+    },
 ]
-
 
 
 def new_expense(*args):
     infos = prompt(expense_questions)
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
-    print("Expense Added !")
+    toSave = [infos["amount"], infos["label"], infos["spender"], infos["involved_users"]]
+    with open("expense_report.csv", mode="a", newline="")as file:
+        writer = csv.writer(file)
+        writer.writerow(toSave)
+    print("A new expense has been created !")
     return True
 
 
--- user.py
+++ user.py
@@ -1,8 +1,31 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"name",
+        "message":"New User - Name: ",
+    },
 ]
 
-def add_user():
+
+def chose_user(*args):
+    with open("users.csv", mode="r") as file:
+        reader = csv.reader(file)
+        users = []
+        for row in reader:
+            users.append(row[0])
+    return users
+
+
+def add_user(*args):
     # This function should create a new user, asking for its name
-    return
+    infos = prompt(user_questions)
+    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    toSave = [infos["name"]]
+    with open("users.csv", mode="a", newline="")as file:
+        writer = csv.writer(file)
+        writer.writerow(toSave)
+    print("A new user has been created !")
+    return True
